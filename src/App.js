3b34ead5a706c7c030569d41f1e7dbfcf4413d1a
Morgane-Straw <58706260+Morgane-Straw@users.noreply.github.com>
2021-05-12 11:50:55 +0200

    pouet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Pouet</p>
         <a
           className="App-link"
           href="https://reactjs.org"
