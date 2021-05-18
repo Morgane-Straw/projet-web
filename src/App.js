07c0fd9758b8677b2719fa24efd1acff5bfe928d
Morgane-Straw <58706260+Morgane-Straw@users.noreply.github.com>
2021-05-18 13:22:17 +0200

    Parse API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import React from 'react';
 import Home from './components/home/Home';
 import Profil from './components/profil/Profil';
 import Connexion from './components/connexion/Connexion';
 import Inscription from './components/inscription/Inscription';
+import Data from './components/data/Data';
 import reportWebVitals from './reportWebVitals';
 
 import {Route, Link} from 'react-router-dom';
@@ -14,7 +16,7 @@ function App() {
       <Route exact path="/profil" component= {Profil}/>
       <Route exact path="/connexion" component= {Connexion}/>
       <Route exact path="/inscription" component= {Inscription}/>
-
+      <Route exact path="/data" component= {Data}/>
       <footer>
       <div className="footer" role="group" aria-label="Basic example">
                 <button type="button" className="btn btn-light">Accueil</button>
