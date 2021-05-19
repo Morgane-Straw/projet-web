654f61366b6dccef47f082f9fc5d1fef6fb0d903
ValentineDssn <47398481+ValentineDssn@users.noreply.github.com>
2021-05-19 22:50:28 +0200

    test css exercices

--- src/components/accueil/Accueil.js
+++ src/components/accueil/Accueil.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbarre from '..//navbarre/Navbarre';
+import sun_logo from '../../sun.png';
 import './Accueil.css';
 import bibo from '../../Bibo.png';
 
@@ -14,8 +15,9 @@ export default class Accueil extends React.Component{
                     <img src={bibo} className="Bibo_accueil" alt="bibo" /> 
                     <p className="Text_Bonjour">Bonjour</p>
                     <p className="Text_Humeur">Comment allez-vous aujourd'hui ?</p>
+                    <img src={sun_logo} className="Sun_Logo" alt="sun_logo" />
                     <div className="Humeur">
-                     blabla
+                        <p className="Text_Humeur_2">Vous avez l'air de bonne humeur !</p>
                     </div>
                 </div>
                 <Navbarre/>
--- src/components/exercice/Exercice.js
+++ src/components/exercice/Exercice.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbarre from '../navbarre/Navbarre';
+import sunset_image from '../../sunset.jpg';
 import './Exercice.css';
 
 export default class Exercice extends React.Component{
@@ -10,6 +11,16 @@ export default class Exercice extends React.Component{
         return(
             <div className="Page_Normale"> 
                <h1>Exercices</h1>
+               <div className="Images_Exercice">
+               <img src={sunset_image} className="Sunset_Image" alt="sunset_image" />
+               <img src={sunset_image} className="Sunset_Image" alt="sunset_image" />
+               </div>
+
+
+               <div className="Images_Exercice">
+               <img src={sunset_image} className="Sunset_Image" alt="sunset_image" />
+               <img src={sunset_image} className="Sunset_Image" alt="sunset_image" />
+               </div>
                <Navbarre/>
             </div>
         );
