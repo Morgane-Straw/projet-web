0fd30b0a581b2cbc50f8de65f8c7e025408c654e
ValentineDssn <47398481+ValentineDssn@users.noreply.github.com>
2021-05-20 13:45:47 +0200

    Exercices

--- src/components/exercice/Exercice.js
+++ src/components/exercice/Exercice.js
@@ -2,10 +2,11 @@ import React from "react";
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbarre from '../navbarre/Navbarre';
-import sunset_image from '../../asset/sunset.jpg';
-import arbre_image from '../../asset/arbre.jpg';
-import ocean_image from '../../asset/ocean.jpg';
-import prairie_image from '../../asset/prairie.jpg';
+import sunset_image from '../../asset/sunset.png';
+import arbre_image from '../../asset/arbre.png';
+import ocean_image from '../../asset/ocean.png';
+import prairie_image from '../../asset/prairie.png';
+import meditation_image from '../../asset/meditation.png';
 import {Route, Link} from 'react-router-dom';
 
 
@@ -24,7 +25,7 @@ export default class Exercice extends React.Component{
                <div className="Images_Exercice">
                 <Link to="/exempleexercice"><img src={arbre_image} className="Sunset_Image" alt="sunset_image" /></Link>
                 <Link to="/exempleexercice"><img src={sunset_image} className="Sunset_Image" alt="sunset_image" /></Link>
-                <Link to="/exempleexercice"><img src={sunset_image} className="Sunset_Image" alt="sunset_image" /></Link>
+                <Link to="/exempleexercice"><img src={meditation_image} className="Sunset_Image" alt="meditation_image" /></Link>
                </div>
          
                <div className="Type_Exercice">
@@ -35,7 +36,6 @@ export default class Exercice extends React.Component{
                <div className="Images_Exercice2">
                <Link to="/exempleexercice"><img src={prairie_image} className="Sunset_Image" alt="prairie_image" /></Link>
                <Link to="/exempleexercice"><img src={ocean_image} className="Sunset_Image" alt="ocean_image" /></Link>
-               <Link to="/exempleexercice"><img src={sunset_image} className="Sunset_Image" alt="sunset_image" /></Link>
                </div>
                <Navbarre/>
             </div>
